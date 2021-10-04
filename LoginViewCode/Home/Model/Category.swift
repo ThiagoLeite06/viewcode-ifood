3fbe07c2b95ccd542a847b3687fdad53fb115aa5
thiagoleite6 <thiago.0686@gmail.com>
2021-10-04 14:26:33 -0300

    Add CollectionViewCell customizada.

---
+++ LoginViewCode/Home/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  LoginViewCode
+//
+//  Created by Thiago Leite on 04/10/21.
+//
+
+import Foundation
+
+struct Category {
+    var name: String
+    var nameImage: String
+}
